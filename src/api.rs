2142f1dd670c3ec885b5f44bc3dea1c1df011b66
Daniel Tebbutt <daniel@shift72.com>
2024-07-20 23:29:18 +1200

    wakeup first commit

---
+++ src/api.rs
@@ -0,0 +1,167 @@
+use core::str::from_utf8;
+
+use cyw43_pio::PioSpi;
+use defmt::{info, unwrap, warn};
+use embassy_executor::Spawner;
+use embassy_net::{tcp::TcpSocket, Config, Stack, StackResources};
+use embassy_rp::{
+    clocks::RoscRng,
+    gpio::{Level, Output},
+    peripherals::{DMA_CH1, PIN_23, PIN_24, PIN_25, PIN_29, PIO1},
+    pio::Pio,
+};
+use embassy_time::{Duration, Timer};
+use embedded_io_async::Write;
+use rand::RngCore;
+use static_cell::StaticCell;
+
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<
+        'static,
+        Output<'static, PIN_23>,
+        PioSpi<'static, PIN_25, PIO1, 0, DMA_CH1>,
+    >,
+) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
+    stack.run().await
+}
+
+use crate::Irqs;
+
+#[embassy_executor::task]
+pub(crate) async fn serve(
+    spawner: Spawner,
+    pio_a: PIO1,
+    dma: DMA_CH1,
+    pwr_pin: PIN_23,
+    cs_pin: PIN_25,
+    spi1_pin: PIN_24,
+    spi2_pin: PIN_29,
+) {
+    let mut rng = RoscRng;
+
+    let wifi_network = env!("WIFI_NETWORK");
+    let wifi_password = env!("WIFI_PASSWORD");
+
+    let fw = include_bytes!("../cyw43/43439A0.bin");
+    let clm = include_bytes!("../cyw43/43439A0_clm.bin");
+
+    // To make flashing faster for development, you may want to flash the firmwares independently
+    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
+    //     probe-rs download 43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs download 43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
+    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
+    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+
+    let pwr = Output::new(pwr_pin, Level::Low);
+    let cs = Output::new(cs_pin, Level::High);
+    let mut pio = Pio::new(pio_a, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        spi1_pin,
+        spi2_pin,
+        dma,
+    );
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let config = Config::dhcpv4(Default::default());
+    //let config = embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+    //    address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 69, 2), 24),
+    //    dns_servers: Vec::new(),
+    //    gateway: Some(Ipv4Address::new(192, 168, 69, 1)),
+    //});
+
+    // Generate random seed
+    let seed = rng.next_u64();
+
+    // Init network stack
+    static STACK: StaticCell<Stack<cyw43::NetDriver<'static>>> = StaticCell::new();
+    static RESOURCES: StaticCell<StackResources<2>> = StaticCell::new();
+    let stack = &*STACK.init(Stack::new(
+        net_device,
+        config,
+        RESOURCES.init(StackResources::<2>::new()),
+        seed,
+    ));
+
+    unwrap!(spawner.spawn(net_task(stack)));
+
+    loop {
+        //control.join_open(WIFI_NETWORK).await;
+        match control.join_wpa2(wifi_network, wifi_password).await {
+            Ok(_) => break,
+            Err(err) => {
+                info!("join failed with status={}", err.status);
+            }
+        }
+    }
+
+    // Wait for DHCP, not necessary when using static IP
+    info!("waiting for DHCP...");
+    while !stack.is_config_up() {
+        Timer::after_millis(100).await;
+    }
+    info!("DHCP is now up!");
+
+    // And now we can use it!
+
+    let mut rx_buffer = [0; 4096];
+    let mut tx_buffer = [0; 4096];
+    let mut buf = [0; 4096];
+
+    loop {
+        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+        socket.set_timeout(Some(Duration::from_secs(10)));
+
+        control.gpio_set(0, false).await;
+        info!("Listening on TCP:1234...");
+        if let Err(e) = socket.accept(1234).await {
+            warn!("accept error: {:?}", e);
+            continue;
+        }
+
+        info!("Received connection from {:?}", socket.remote_endpoint());
+        control.gpio_set(0, true).await;
+
+        loop {
+            let n = match socket.read(&mut buf).await {
+                Ok(0) => {
+                    warn!("read EOF");
+                    break;
+                }
+                Ok(n) => n,
+                Err(e) => {
+                    warn!("read error: {:?}", e);
+                    break;
+                }
+            };
+
+            info!("rxd {}", from_utf8(&buf[..n]).unwrap());
+
+            match socket.write_all(&buf[..n]).await {
+                Ok(()) => {}
+                Err(e) => {
+                    warn!("write error: {:?}", e);
+                    break;
+                }
+            };
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+#![no_std]
+#![no_main]
+
+mod api;
+mod pio_ws2812;
+
+use {
+    core::iter::repeat,
+    defmt::*,
+    defmt_rtt as _,
+    embassy_executor::Spawner,
+    embassy_rp::{
+        bind_interrupts,
+        gpio::{Input, Level, Output, Pull},
+        peripherals::{DMA_CH0, PIN_11, PIN_3, PIN_5, PIO0, PIO1},
+        pio::{InterruptHandler, Pio},
+    },
+    embassy_time::{Duration, Ticker, Timer},
+    panic_probe as _,
+    pio_ws2812::Ws2812,
+    smart_leds::{brightness, gamma, RGB8},
+};
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+    PIO1_IRQ_0 => InterruptHandler<PIO1>;
+});
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    info!("Start");
+    let p = embassy_rp::init(Default::default());
+
+    spawner
+        .spawn(heartbeat(p.PIO0, p.DMA_CH0, p.PIN_5))
+        .unwrap();
+    spawner.spawn(watch_usr_key(p.PIN_11, p.PIN_3)).unwrap();
+    spawner
+        .spawn(api::serve(
+            spawner, p.PIO1, p.DMA_CH1, p.PIN_23, p.PIN_25, p.PIN_24, p.PIN_29,
+        ))
+        .unwrap();
+}
+
+#[embassy_executor::task]
+async fn watch_usr_key(p: PIN_11, power_on: PIN_3) {
+    let mut pin = Input::new(p, Pull::Up);
+    let mut toggle = Level::Low;
+    let mut power_on = Output::new(power_on, toggle);
+
+    loop {
+        pin.wait_for_falling_edge().await;
+        info!("USR key pressed");
+        toggle = if toggle == Level::High {
+            Level::Low
+        } else {
+            Level::High
+        };
+        power_on.set_level(toggle);
+
+        Timer::after(Duration::from_millis(1500)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn heartbeat(pio: PIO0, dma: DMA_CH0, pin: PIN_5) {
+    const NUM_LEDS: usize = 144;
+    let mut data = [RGB8::default(); NUM_LEDS];
+
+    let Pio {
+        mut common, sm0, ..
+    } = Pio::new(pio, Irqs);
+    let mut ws2812 = Ws2812::new(&mut common, sm0, dma, pin);
+
+    // Loop forever making RGB values and pushing them out to the WS2812.
+    let mut ticker = Ticker::every(Duration::from_millis(10));
+    loop {
+        for j in (0..255).chain((0..255).rev()) {
+            gamma(brightness(
+                repeat(RGB8::new(200, 130, 50)).take(NUM_LEDS),
+                j,
+            ))
+            .enumerate()
+            .for_each(|(i, d)| data[i] = d);
+            ws2812.write(&data).await;
+
+            ticker.next().await;
+        }
+    }
+}
